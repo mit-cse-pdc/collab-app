e41a0ef59982e2ee954ea94065f58b88af6253f5
NotLester <lester.lewisudupi@gmail.com>
2025-02-25 18:07:33 +0530

    refactor package structure to edu.manipal.cse for LectureServiceApplication and its tests

--- lecture-service/src/main/java/edu/manipal/cse/lectureservice/LectureServiceApplication.java
+++ lecture-service/src/main/java/edu/manipal/cse/lectureservice/LectureServiceApplication.java
@@ -1,4 +1,4 @@
-package com.pdc.lectureservice;
+package edu.manipal.cse.lectureservice;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
--- lecture-service/src/test/java/edu/manipal/cse/lectureservice/LectureServiceApplicationTests.java
+++ lecture-service/src/test/java/edu/manipal/cse/lectureservice/LectureServiceApplicationTests.java
@@ -1,4 +1,4 @@
-package com.pdc.lectureservice;
+package edu.manipal.cse.lectureservice;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
